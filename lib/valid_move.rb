2c8e7b6d40734bb0179d887875432565b6c61810
aviflombaum <avi@flombaum.com>
2015-09-04 20:06:55 -0400

    Initial solution

---
+++ lib/valid_move.rb
@@ -0,0 +1,13 @@
+def valid_move?(board, position)
+  if position.to_i.between?(1,9) 
+    if !position_taken?(board, position.to_i-1)
+      true
+    end
+  end
+
+  # position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
+end
+
+def position_taken?(board, location)
+  !(position(board, location).nil? || position(board, location) == " ")
+end
