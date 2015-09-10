04d6a4e1599aee19fd1a8b459cfbc632217edd58
aviflombaum <avi@flombaum.com>
2015-09-09 21:05:55 -0400

    starter specs

--- spec/valid_move_spec.rb
+++ spec/valid_move_spec.rb
@@ -1,2 +1,19 @@
 require_relative "../lib/valid_move.rb"
 
+describe './lib/valid_move.rb' do
+  # Needs more specs
+
+  it 'returns true for a valid position on an empty board' do
+    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    position = "1"
+    expect(valid_move?(board, position)).to be_truthy
+  end
+
+  it 'returns false for an occupied position' do
+    board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+    position = "5"
+    
+    expect(valid_move?(board, position)).to be_falsey
+  end
+
+end
