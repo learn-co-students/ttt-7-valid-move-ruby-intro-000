ebd6fbf48778ebf5d48b80ed0df967e33c72e8f2
Brian Holland <beehollander@gmail.com>
2016-02-06 17:47:51 +0000

    edit README for clarity, third test entry: 100 -> '100'

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,6 +8,6 @@ def valid_move?(board, position)
   position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
 end
 
-def position_taken?(board, location)
-  board[location] != " "
+def position_taken?(board, index)
+  board[index] != " "
 end
