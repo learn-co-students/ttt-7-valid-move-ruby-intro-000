9f124d35f1b8abcb0a6e374d8db163a9c7b318c1
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-05-13 14:59:24 -0400

    Update solution file to be inline with index tests

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,11 @@
-def valid_move?(board, position)
-  # if position.to_i.between?(1,9)
-  #   if !position_taken?(board, position.to_i-1)
+def valid_move?(board, index)
+  # if index.between?(1,9)
+  #   if !position_taken?(board, index)
   #     true
   #   end
   # end
 
-  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def position_taken?(board, index)
