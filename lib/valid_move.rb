ada967243111045c55bdad5026c5c4f281287ea6
Clare Horton <clare.e.horton@gmail.com>
2016-08-04 06:50:36 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,11 @@
-# code your #valid_move? method here
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && (board[index] == " " || board[index] == "" || board[index] == nil)
+    return true
+  else
+    return false
+  end
+end
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+# copy pasta-ed #position_taken? from previous lab, except for reversing the true/false returns
