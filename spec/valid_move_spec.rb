5359e1d6c5b917040fee26be51f5c3f665351682
Armaan Dhanji <dhanjiarmaan@gmail.com>
2016-08-12 17:16:07 +0000

    Done.

--- spec/valid_move_spec.rb
+++ spec/valid_move_spec.rb
@@ -21,10 +21,4 @@ describe './lib/valid_move.rb' do
 
     expect(valid_move?(board, index)).to be_falsey
   end
-
-  it "says hello" do
-    board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-    index = 4
-    expect (valid_move?(board,index)).to output("true").to_stdout
-  end
 end
