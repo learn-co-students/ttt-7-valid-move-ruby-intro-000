0315e50f61c3f766e7ec723aa4b5ff34944a58d4
Senad Hadrovic <senadhadrovic@gmail.com>
2016-09-11 16:20:27 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,7 @@ end
 
 
 def valid_move?(board, index)
-  if position_taken? == false && index.between?(0, 8)
+  if position_taken?(board, index) == false && index.between?(0, 8)
     return true
   else
     return false
