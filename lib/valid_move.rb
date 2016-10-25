2d9638682925209103b1a70c91a794f59d5f50e1
Michal Vacek <vahyno@gmail.com>
2016-10-25 20:02:18 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,12 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  #if board[index] == " " || board[index] == "" || board[index] == nil && index == (0..8)
-  #true
-  #else
-  #false
-  #end
-#end
-
-  board[index] == " " || board[index] == "" || board[index] == nil && index == (0..8)?
-  true : false
+  if board[index] == " " || board[index] == "" || board[index] == nil && index == (0..8)
+  true
+  else
+  false
+  end
 end
+
+#  board[index] == " " || board[index] == "" || board[index] == nil && index == (0..8)?
+#  true : false
+#end
