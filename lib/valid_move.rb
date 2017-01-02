4eaa34bf33e467909a9c2251784179985ffc8881
Ben Jay <fakebenjay@gmail.com>
2017-01-02 08:19:15 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 def valid_move?(board, index)
 
-  if board[index] == " " || board[index] == "" || board[index] == nil && index == be_between?(0, 8)
+  if board[index] == " " || board[index] == "" || board[index] == nil && index.between?(0, 8)
     return true
 
   else
