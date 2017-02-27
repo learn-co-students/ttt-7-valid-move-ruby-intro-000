c9ccd7ece436b8fdc44e3c395d627a6aab1cb95d
Makai Post <postmakai@gmail.com>
2017-02-27 01:07:04 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,6 @@
 # code your #valid_move? method here
-
+def valid_move?(board, index)
+  index >= 0 && index < 9 && board[index] == " "
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
