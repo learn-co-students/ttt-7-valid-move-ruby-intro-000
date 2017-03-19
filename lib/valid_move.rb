6a02aa49d76638058d67943b60a5463da5d242b1
Tyler Knight <tknightx31@gmail.com>
2017-03-19 22:36:04 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,15 @@
 # code your #valid_move? method here
-
+def valid_move?(arr, idx)
+  if (0..8).include?(idx) && !(position_taken?(arr, idx))
+    return true
+  end
+  false
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(arr, idx)
+  if arr[idx] == "X" || arr[idx] == "O"
+    return true
+  end
+  false
+end
