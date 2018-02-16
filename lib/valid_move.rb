f4fee79b9d329b14490a4db6799ff2ba1be2923a
Gerardo Dueñas <geras998@gmail.com>
2018-02-16 02:57:57 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,7 @@
 def valid_move?(board, index)
-  return !position_taken?(board, index) && board.length > index
+  !position_taken?(board, index) && board.length > index
 end
 
 def position_taken?(board, index)
-  return false if board[index] == " " or board[index].to_s.empty?
-  return true if board[index].eql?("X") or board[index].eql?("O")
+  board[index] != " "
 end
