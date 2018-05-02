59e07563fb3c2e16f39fa643b9f3de73280b7548
growhan@gmail.com Sinha <growhan@gmail.com>
2018-05-02 14:50:07 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,17 @@
 # code your #valid_move? method here
-
+def valid_move?(array, inp)
+  if (!(position_taken?(array, inp)) && inp >= 0 && inp <= 8)
+    return true
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(array, inp)
+  if array[inp] == "X" || array[inp] == "O"
+    return true
+  elsif array[inp] == " " || array[inp] == ""
+    return false
+  else
+    return false
+  end
+end
