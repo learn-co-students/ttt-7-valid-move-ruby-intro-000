e3a666a1a813226218955b9ab046e3ab836a449c
Morgan Stanley <morganpstanley@gmail.com>
2019-06-15 03:49:25 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index.between?(1, 9) && !position_taken?(board, index)
+  if index.between?(0, 9) && !position_taken?(board, index)
     return true
   else
     return false
--- spec/valid_move_spec.rb
+++ spec/valid_move_spec.rb
@@ -2,9 +2,9 @@ require_relative "../lib/valid_move.rb"
 
 describe './lib/valid_move.rb' do
   it 'returns true for a valid position on an empty board' do
-    board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
+    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     index = 0
-    expect(valid_move?(board, index)).to be_falsey
+    expect(valid_move?(board, index)).to be_truthy
   end
 
   it 'returns true for a valid position on a non-empty board' do
