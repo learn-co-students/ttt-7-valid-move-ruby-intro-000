816ce0151ddaacef46d5d51f3923578ab860fffd
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-12 09:00:32 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move? (board, index)
-  if(position_taken?(board,index) || index < 0 || index > 8)
+  if(position_taken?(board,index) || !index.between?(0,8))
     return false
   else
     return true
