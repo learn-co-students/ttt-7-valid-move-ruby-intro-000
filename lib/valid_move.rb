3798a7b8856c43aec6d7a484517e3ff331456ee0
Gordon Gyakyi <gordon.gyakyi@btinternet.com>
2019-08-04 16:40:32 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,22 @@
 # code your #valid_move? method here
 
+def valid_move?(board, index)
+=begin
+this method also works re passing the tests, however, it does not do so by calling the position_taken? method
+and so the other code is better
+=end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+  #index.between?(0, 8) && board[index] == " "
+
+#this wirks using the position_taken? method but using "!" to reverse the use.  note how you call the methos inside this one
+  index.between?(0, 8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+
+if board[index] != " " && board[index] != "" && board[index] == "nil"
+else
+  board[index] == "X" || board[index] == "O"
+
+end
+end
