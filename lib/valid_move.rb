ee16dc30f044850a290cb871e5f935ce7f0ac7b9
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-08-06 00:07:51 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
-# code your #valid_move? method here
-
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, index)
+  position = board[index]
+  if position.nil?
+    false
+  elsif position_taken?(board, index)
+    false
+  else
+    true
+  end
+end
+def position_taken?(board, index)
+  position = board[index]
+  if position.nil?
+    false
+  elsif position.upcase != "X" and position.upcase != "O"
+    false
+  else
+    true
+  end
+end
