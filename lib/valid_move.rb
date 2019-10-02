53dffdbb5c9755bbce1e0bc6162043d402623cf8
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-02 14:36:30 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,20 +1,14 @@
-# code your #valid_move? method here
-def valid_move?(move, board)
-  if position_taken?(board, move) == false && move.between?(0,8)
- #   TRUE
-#    else
-#      false
-    end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0,8)
+    return true
   end
+end
     
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-# def position_taken?(board, index)
-#   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-#      return false 
-#   elsif 
-#     boad[move] == "X" || board[move] == "O"
-#     return true
-# end
-# end
-
+ def position_taken?(board, index)
+   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+      return false 
+   end
+ end
