a217990a27b09d4f3305c49e5d0c4f970df78c2e
Patrick Colina <trick.co@yahoo.com>
2019-12-15 22:34:13 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,11 @@
-# code your #valid_move? method here
+require_relative '../lib/position_taken.rb'
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, index)
+  if index.between?(-1, 9) != true
+    false
+  elsif position_taken?(board, index) == true
+    false
+  elsif position_taken?(board, index) == false
+    true
+  end
+end
