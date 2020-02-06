03879d0dbc6b5ac85f9dd5d70d95bd2836c89570
Anh Vuong <vuonga1103@yahoo.com>
2020-02-06 21:19:11 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,10 @@
-# code your #valid_move? method here
+def valid_move?(board, index)
+  #for it to be a valid move, the position must be empty AND the index has to be between 0-8
 
+  if ((board[index] == "") || (board[index] == " ") || (board[index] == nil) && (index.between?(0, 8)))
+    return true
+  end
+  return false
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
