6d80a97a266dab112d0d0df6855d81044bfb95ea
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-03-01 21:03:02 +0000

    Done.

--- spec/valid_move_spec.rb
+++ spec/valid_move_spec.rb
@@ -23,7 +23,6 @@ describe './lib/valid_move.rb' do
   it 'returns nil or false for a position that is not on the board' do
     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     index = 100
-
     expect(valid_move?(board, index)).to be_falsey
   end
 end
