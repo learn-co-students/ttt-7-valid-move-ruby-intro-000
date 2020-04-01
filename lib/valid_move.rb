6f33ed4bdfb0a442e45a4f88c0c713c44c88aebd
plucky-mapping-0074 <joshuadelapenha@yahoo.com>
2020-04-01 17:46:53 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+  if position_taken?(board,index)
+    false
+  elsif index.between?(0, 8)
+    true
+  else
+    false
+  end
 
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+    if board[index] == "" || board[index] == " " || board[index] == nil
+      return false
+    else
+      return true
+    end
+end
