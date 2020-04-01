31f1b8e030618b2b5592051c8b4d18e678f5150e
Shadman Ahmed <shadman@outlook.com>
2020-04-01 23:44:48 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,17 +1,25 @@
-def valid_move? (board,index)
-  spot = position.to_i - 1
-if !position_taken? (board,index) && spot.between? (0,8)
-  return true
-else
-  return false
-end
-end
-
-def position_taken? (board,index)
-  if board[position] == " " || board[position] == "" || board[position] == nil
-return false
-  else
-return true
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
   end
+
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+
 
 end
