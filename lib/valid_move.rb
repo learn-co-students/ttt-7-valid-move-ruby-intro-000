c022502751994d021d8fba7ea96f9c492308150f
decent-android-7966 <garrettsng@gmail.com>
2020-04-07 23:22:15 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,15 @@
 # code your #valid_move? method here
-
+def valid_move?(board, index)
+  if index >= 0 && index <= 8 && position_taken?(board, index) == false
+      true
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  else
+    false
+  end
+end
