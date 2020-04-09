8e19af6fb5a695c467f8a2de39ae38659cbb16fe
github username <github email address>
2020-04-08 20:06:33 -0700

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,7 @@
-# code your #valid_move? method here
-
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, index)
+    index.between?(0,8) && !position_taken?(board, index)
+  end
+  
+  def position_taken?(board, index)
+    board[index] != " "
+  end
