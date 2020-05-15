75c5a6c71fec4ce7a9db401fb594fef28ee43250
Donat Pllana <donat.pllana@outlook.com>
2020-05-15 07:48:39 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,8 +10,6 @@ end
 def valid_move?(board,index)
   if(!position_taken?(board,index) && index.between?(0,8))
     true
-  elsif (position_taken?(board,index) && !index.between?(0,8))
-    false
   end
 end
 
