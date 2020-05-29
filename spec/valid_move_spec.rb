edb8b9a8c8740a17748d585644cb0c8aaa81cbce
KHANDKER HASSAN <khassanasgar@gmail.com>
2020-05-29 02:09:30 +0000

    Done.

--- spec/valid_move_spec.rb
+++ spec/valid_move_spec.rb
@@ -1,15 +1,10 @@
 require_relative "../lib/valid_move.rb"
 
 describe './lib/valid_move.rb' do
+
   it 'returns true for a valid position on an empty board' do
     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     index = 0
-    expect(valid_move?(board, index)).to be_truthy
-  end
-
-  it 'returns true for a valid position on a non-empty board' do
-    board = [" ", " ", "X", " ", " ", " ", " ", "O", " "]
-    index = 5
     expect(valid_move?(board, index)).to be_truthy
   end
 
@@ -26,4 +21,5 @@ describe './lib/valid_move.rb' do
 
     expect(valid_move?(board, index)).to be_falsey
   end
+
 end
