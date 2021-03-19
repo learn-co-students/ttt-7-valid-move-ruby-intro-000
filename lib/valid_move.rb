4b7898949af03f1f45effb48f31aa51e04d5200d
Michael Raysor <michaelraysor1@gmail.com>
2021-03-19 01:06:19 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,10 @@
 # code your #valid_move? method here
-
-
+def valid_move? (board,index)
+if board[index] == "x" || board[index] == "O" ||
+   false
+elsif board[index] == " "
+   true
+ 
+end
+end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
