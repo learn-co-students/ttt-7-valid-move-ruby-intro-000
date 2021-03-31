65cc2a8bddc632b5c085ce64cf6d808ed26dc6d2
Kibin <jacobyoo00@hotmail.com>
2021-03-31 23:02:34 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
 # code your #valid_move? method here
+# require 'pry'
+def position_taken?(board, index)
+if board[index] == " " || board[index] == "" || board[index] == nil
+  return false
+else
+  return true
+end
+end
 
+def valid_move?(board, index)
+# binding.pry
+if !position_taken?(board, index) && index >= 0 && index <= board.length
+ return true
+else
+  return nil || false
+end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
