1cf8bd34cc366ed0891620e7f4b7a1382e258b4c
Jasmine Alexander <solflair.jewelry@gmail.com>
2021-04-05 18:12:02 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,31 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+  if position_taken?(board, index) || !spot_exists?(board, index)
+    return false
+  else
+    return true
+  end
+
+end
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    return true
+
+  elsif board[index] == " " || board[index] == ""
+    return false
+  end
+end
+
+
+def spot_exists?(board, index)
+  if !(board[index] == nil)
+    return true
+  else
+    return false
+  end
+end
